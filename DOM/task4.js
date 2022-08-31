2dcf1b70df1735a006f28f47ea3099d6a5b85092
ChhaviPrabhakar <prabhakarchhavi9@gmail.com>
2022-08-31 22:27:01 +0530

    by tag name

--- DOM/task4.js
+++ DOM/task4.js
@@ -1,21 +1,38 @@
 //GET ELEMENT BY CLASS NAME//
 
-var items= document.getElementsByClassName('list-group-item');
-// console.log(items);
-// console.log(items[1]);
-// items[1].textContent= 'Changed';
-// items[1].style.fontWeight= 'bold';
-// items[1].style.color= 'red';
-// items[1].style.backgroundColor= 'yellow';
+// var items= document.getElementsByClassName('list-group-item');
+// // console.log(items);
+// // console.log(items[1]);
+// // items[1].textContent= 'Changed';
+// // items[1].style.fontWeight= 'bold';
+// // items[1].style.color= 'red';
+// // items[1].style.backgroundColor= 'yellow';
 
-// for(var i=0; i<items.length; i++)
+// // for(var i=0; i<items.length; i++)
+// // {
+// //     items[i].style.backgroundColor= 'pink';
+// // }
+
+// items[2].style.backgroundColor= 'green';
+
+// for(var i=0;i<items.length;i++)
 // {
-//     items[i].style.backgroundColor= 'pink';
+//     items[i].style.fontWeight= 'bold';
 // }
 
-items[2].style.backgroundColor= 'green';
 
-for(var i=0;i<items.length;i++)
+//GET ITEM BY TAG NAME//
+
+var li= document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent= 'Changed';
+li[1].style.fontWeight= 'bold';
+li[1].style.color= 'red';
+li[1].style.backgroundColor= 'yellow';
+li[2].style.backgroundColor= 'green';
+
+for(var i=0;i<li.length;i++)
 {
-    items[i].style.fontWeight= 'bold';
+    li[i].style.fontWeight= 'bold';
 }
